9aa18f43d6ee21eec5294f2953757eb4b748225c
Eng.Mahmood Anaam <eng.mahmood.anaam@gmail.com>
2024-11-09 07:49:48 +0300

    Update constants.ts

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,6 +1,6 @@
 export const APP_NAME: string = "UFUQ";
 
-const PRODUCTION_DOMAIN: string = "https://ufuq.vercel.app";
+const PRODUCTION_DOMAIN: string = "https://web-ufuq.vercel.app";
 const DEVELOPMENT_DOMAIN: string = "http://localhost:3000";
 
 export const DOMAIN: string =
